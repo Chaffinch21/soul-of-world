f47cd5454318d21fdd6bf7f92ce9ffab5c354995
Chaffinch21 <lena_431@mail.ru>
2022-06-17 21:22:33 +0700

    маленькая правка

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import loadPhotoReducer from "./loadPhotoReducer";
-import tokenReducer from "./tokenReducer";
 export default combineReducers({
  loadPhotoReducer
 });
